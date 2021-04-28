75f9cb68a58e6070b9f9bb5286c2fb671db198ab
syzygy_608 <47174225+syzygy608@users.noreply.github.com>
2021-04-28 19:18:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ const calculate = e => {
   const radius2 = document.getElementById('radius2').value; 
   const mag = ((8 * 4 * 0.0000001 * 3.14 * num * current)/(radius1 * 11.18));
   const e_and_m = ((2 * volt)/(mag * mag * radius2 * radius2))
-  document.getElementById('results').innerHTML = `<h3 style = "color: white;">Result:</h3><p style = "color: white;">磁場(π = 3.14):${mag.toFixed(10)}</p><p style = "margin-top: 5px;"></p><p style = "color: white;">荷質比(e/m, π = 3.14):${e_and_m.toFixed(10)}</p>`
+  document.getElementById('results').innerHTML = `<h4 style = "color: white;">Result:</h4 ><p style = "color: white;">磁場(π = 3.14):${mag.toFixed(10)}</p><p style = "color: white;">荷質比(e/m, π = 3.14):${e_and_m.toFixed(10)}</p>`
 }
 
